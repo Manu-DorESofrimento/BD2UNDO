2287e213782b29408dfd4d70f06424f072134e6d
Manu <83024290+Manu-DorESofrimento@users.noreply.github.com>
2023-11-09 19:57:42 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#imports
+import psycopg2
+
+from scripts.db_config import db_config
+from scripts.load_database import load_database
+from scripts.log_undo import perform_undo
+
+def main():
+  conn = None
+  try:
+    # conecta com banco de dados e cria cursor
+    params = db_config()
+    conn = psycopg2.connect(**params)
+    cursor = conn.cursor()
+
+	  # carrega o banco
+    load_database(cursor)
+
+    # recuperar log UNDO
+    perform_undo(cursor)
+
+	  # fecha conexão com banco
+    cursor.close()
+
+  except (Exception, psycopg2.DatabaseError) as error:
+    print(error)
+
+  finally:
+    if conn is not None:
+      conn.close()
+
+
+if __name__ == '__main__':
+  main()
